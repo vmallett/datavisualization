cbc945c8a9f38a403ad309a1ddeb637d7d8332ec
Victoria Mallett Rachleff <46464020+vmallett@users.noreply.github.com>
2024-05-24 13:38:36 -0700

    correcting html height pixel

--- streamlit/alternate_streamlit.py
+++ streamlit/alternate_streamlit.py
@@ -130,7 +130,7 @@ st.header("test html import")
 HtmlFile = open("html/MTG_INFERENCE 1.html", 'r', encoding='utf-8')
 source_code = HtmlFile.read() 
 print(source_code)
-components.html(source_code)
+components.html(source_code,  height = 600)
 
 
 
