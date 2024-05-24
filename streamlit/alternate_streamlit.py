a53e0715a94cea6983b9f7fad4608facb360347d
Victoria Mallett Rachleff <46464020+vmallett@users.noreply.github.com>
2024-05-24 12:20:33 -0700

    removing cv2

--- streamlit/alternate_streamlit.py
+++ streamlit/alternate_streamlit.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import altair as alt
 from streamlit_image_zoom import image_zoom
 from PIL import Image
-import cv2
+# import cv2
 from typing import Union, Optional, Tuple, NewType
 from IPython.display import HTML
 import sys
